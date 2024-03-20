2993260ec81bf860ede6bb91d9ce2b7e3ccdbfea
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2024-03-20 22:48:05 +0200

    changes

--- src/middleware/UserMiddleware.ts
+++ src/middleware/UserMiddleware.ts
@@ -1,18 +1,17 @@
 import { NextFunction, Response } from "express";
 // import { UserDocument } from "../model/userMode";
 export const isAdmin = async (req: any, res: Response, next: NextFunction) => {
-    const currentUser = req.currentUser;
-    try {
-        if (currentUser.isAdmin) {
-            next();
-        } else {
-            return res.status(401).json({
-                status: 401,
-                message: "This Action is only performed by Admin"
-            })
-        }
-    } catch (error:any) {
-        return res.status(401).json({ error: error.message });
-    }
-    
+	const currentUser = req.currentUser;
+	try {
+		if (currentUser.isAdmin) {
+			next();
+		} else {
+			return res.status(401).json({
+				status: 401,
+				message: "This Action is only performed by Admin",
+			});
+		}
+	} catch (error: any) {
+		return res.status(401).json({ error: error.message });
+	}
 };
