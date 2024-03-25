ec46e69941f52b5952a75a7eb93b3bc8bb289eb2
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2024-03-25 19:41:28 +0200

    naming conversation

--- src/route/blogRoute.ts
+++ src/route/blogRoute.ts
@@ -6,12 +6,12 @@ import uploadService from "../service/multer";
 import { isAdmin } from "../middleware/UserMiddleware";
 
 const blogRoutes = express.Router();
-blogRoutes.post("/blog", uploadService.single("image"), Jwt.tokenValidation, isAdmin, blogsController.create_blogs);
+blogRoutes.post("/blogs", uploadService.single("image"), Jwt.tokenValidation, isAdmin, blogsController.create_blogs);
 
 blogRoutes.get("/blogs", blogsController.getAllBlogs);
-blogRoutes.get("/blog/:id", blogsController.getSingleBlog);
-blogRoutes.patch("/blog/:id", blogsController.updatedBlogs);
-blogRoutes.delete("/blog/:id", blogsController.removeBlogs);
+blogRoutes.get("/blogs/:id", blogsController.getSingleBlog);
+blogRoutes.patch("/blogs/:id", blogsController.updatedBlogs);
+blogRoutes.delete("/blogs/:id", blogsController.removeBlogs);
 
 
 
